b69e1955ba8ec375b7fb4116a03d0c34bc09bd97
DerpyNewbie <contact@derpynewbie.dev>
2026-04-06 16:15:21 +0900

    Update booth description

--- src/pages/booth.js
+++ src/pages/booth.js
@@ -2,8 +2,8 @@ import RedirectPage from "../components/RedirectPage";
 
 export default function Redirect() {
     return (<RedirectPage
-        title="Booth"
-        description="CCC OFFICIAL SHOP (BOOTH)"
+        title="CCC OFFICIAL SHOP (BOOTH)"
+        description="カラーバンドなど、サバゲー改変に使える物を配布販売している公式BOOTHです。"
         redirect="https://cccofficialshop.booth.pm/"
     />)
 }
