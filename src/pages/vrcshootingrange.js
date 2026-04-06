aeac81e6044853f916a3d8b53b0401e650f12a0e
DerpyNewbie <contact@derpynewbie.dev>
2026-04-06 16:11:13 +0900

    Specify large image for vrcshootingrange

--- src/pages/vrcshootingrange.js
+++ src/pages/vrcshootingrange.js
@@ -4,6 +4,7 @@ export default function Redirect() {
     return (<RedirectPage
         title="VRSGF_Centurion シューティングレンジ"
         description="VRSGF_Centurion シューティングレンジ by CenturionSystem"
+        summary="summary_large_image"
         image="https://centurioncc.org/img/shooting-range-thumbnail.png"
         alt="A preview image of VRChat world 'VRSGF_Centurion シューティングレンジ'"
         redirect="https://vrchat.com/home/world/wrld_a7ad59a4-38b4-4413-aab4-848a38d6fb48"/>)
