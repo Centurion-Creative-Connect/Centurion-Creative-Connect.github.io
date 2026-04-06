6e05834beedceee560f9fb5e5763f53eb4ce69ee
DerpyNewbie <contact@derpynewbie.dev>
2026-04-06 16:22:07 +0900

    Add x redirect

---
+++ src/pages/x.js
@@ -0,0 +1,9 @@
+import RedirectPage from "../components/RedirectPage";
+
+export default function Redirect() {
+    return (<RedirectPage
+        title="X @VRSGF_Centurion"
+        description="VRサバイバルゲームフィールド-Centurion 隔週金曜日開催 (X / Twitter)"
+        redirect="https://twitter.com/VRSGF_Centurion"
+    />)
+}
